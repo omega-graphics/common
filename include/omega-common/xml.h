371364a189f5c271c4a5e2d980f6fccf388d4bec
Pedestria <42527102+Pedestria@users.noreply.github.com>
2021-06-19 01:09:38 -0400

    xml parser init

---
+++ include/omega-common/xml.h
@@ -0,0 +1,38 @@
+#include "utils.h"
+
+#ifndef OMEGA_COMMON_XML_H
+#define OMEGA_COMMON_XML_H
+
+namespace OmegaCommon {
+    class XMLParser;
+    class XMLSerializer;
+
+    class XML {
+        String tag;
+        Map<String,String> tagProps;
+        Vector<XML> children;
+
+        static std::unique_ptr<XMLParser> parser;
+
+        static std::unique_ptr<XMLSerializer> serializer;
+
+        static XML __parse(std::istream & in);
+        static void __serialize(XML &xml,std::ostream & out);
+    public:
+        String & getName();
+        String & getProp(String & propName);
+        Vector<XML> & getChildren();
+
+        static XML parse(String str);
+        static XML parse(std::istream & is);
+    };
+
+    // class XMLDocument {
+
+    // public:
+    //     static XMLDocument parse(String str);
+    //     static XMLDocument parse(std::istream & is);
+    // };
+}
+
+#endif
